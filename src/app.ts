4c9b08cde84e87af245f8ba018b43e48036cea7a
rbonUniverse <robby.zigi@gmail.com>
2023-09-16 20:05:54 +0300

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import axios from "../node_modules/axios/index";
+
+const form = document.querySelector("form") as HTMLFormElement;
+const addressInput = document.getElementById("address") as HTMLInputElement;
+
+const GOOGLE_API_KEY = "YOUR_GOOGLE_API_KEY";
+
+async function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enterAddress = addressInput.value;
+  const { Map } = (await google.maps.importLibrary(
+    "maps"
+  )) as google.maps.MapsLibrary;
+
+  type googleGeocodingResponce = {
+    results: { geometry: { location: { lat: number; lng: number } } }[];
+    status: "OK" | "ZERO_RESULTS";
+  };
+
+  try {
+    const res = await axios.get<googleGeocodingResponce>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enterAddress
+      )}&key=${GOOGLE_API_KEY}`
+    );
+    if (res.data.status !== "OK") {
+      throw new Error("Location not found!!!");
+    }
+    const coordinates = res.data.results[0].geometry.location;
+    const map = new Map(document.getElementById("map"), {
+      center: coordinates,
+      zoom: 8,
+    });
+    new google.maps.Marker({
+      position: coordinates,
+      map: map,
+    });
+  } catch (err) {
+    alert(err);
+    console.log(err);
+  }
+}
+
+form.addEventListener("submit", searchAddressHandler);
